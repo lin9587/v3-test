64ad9f792d2d8ff22f3b1614189ce0a2871e1a8b
lin9587 <lin9587@aliyun.com>
2023-01-26 22:59:49 +0800

    vue3 learn

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// 引入的不再是 Vue 构造函数了，引入的是一个名为 createApp 的工厂函数
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -14,11 +15,19 @@ import 'element-plus/dist/index.css'
 
 import './style.css'
 
+// 创建应用实例对象——app(类似于之前Vue2中的vm，但app比vm更“轻”)
 const app = createApp(App)
+console.log('createApp', app)
 
 app.use(createPinia())
 app.use(router)
 app.use(antd)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
+// 挂载
 app.mount('#app')
+
+// 卸载
+// setTimeout(() => {
+//   app.unmount('#app')
+// }, 2000)
