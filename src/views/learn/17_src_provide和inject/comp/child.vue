08e02e941156175220573e3bb299e5d0aab79eb2
lin9587 <lin9587@aliyun.com>
2023-02-07 19:31:15 +0800

    learn provide inject

---
+++ src/views/learn/17_src_provide和inject/comp/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件（子）</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child',
+}
+</script>
+<style scoped>
+.child {
+  background-color: gray;
+  padding: 10px;
+}
+</style>
---
+++ src/views/learn/17_src_provide和inject/comp/son.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="son">
+    <h3>我是Son组件（孙）</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Son',
+}
+</script>
+<style scoped>
+.son {
+  background-color: gray;
+  padding: 10px;
+}
+</style>
